e0fa7bb4785504c8dda48e9ab35f60d6e550dd52
Daimar <daimarkc@gmail.com>
2023-05-08 17:05:01 -0300

    primeiro commit

---
+++ src/test/java/APITest.java
@@ -0,0 +1,67 @@
+import io.restassured.http.ContentType;
+import static org.hamcrest.Matchers.*;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class APITest {
+    @Test
+    void deveBuscarPersonagem(){
+
+        given()
+                .baseUri("https://jsonplaceholder.typicode.com")
+                .when()
+                .get("/posts")
+                .then()
+                .statusCode(200)
+                .body("$", hasSize(100));
+
+
+    }
+
+    @Test
+    void deveRealizarUmPost(){
+        given()
+                .baseUri("https://jsonplaceholder.typicode.com")
+                .body("{\"title\": \"Post de Teste\", \"body\": \"Descricao post de teste\", \"userId\": 3}")
+                .when()
+                .post("/posts")
+                .then()
+                .statusCode(201);
+
+             }
+
+    @Test
+    void deveRealizarUmPut() {
+        Post post = new Post("titulo teste", "Descricao post teste", 3);
+        given()
+                .baseUri("https://jsonplaceholder.typicode.com")
+                .body(post)
+                .when()
+                .put("/posts/1")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    void v1deveRealizarUmPut(){
+        given()
+                    .baseUri("https://jsonplaceholder.typicode.com")
+                    .body("{\"title\": \"Post de Teste\", \"body\": \"Descricao post de teste\", \"userId\": 3}")
+                    .when()
+                    .put("/posts/1")
+                    .then()
+                    .statusCode(200);
+
+    }
+
+    @Test
+    void deveRealizarUmDelete() {
+        given()
+                .baseUri("https://jsonplaceholder.typicode.com")
+                .when()
+                .delete("/posts/1")
+                .then()
+                .statusCode(200);
+    }
+}
---
+++ src/test/java/Post.java
@@ -0,0 +1,14 @@
+public class Post {
+
+    public String title;
+
+    public String body;
+
+    public Integer userId;
+
+    public Post(String title, String body, Integer userId) {
+        this.title = title;
+        this.body = body;
+        this.userId = userId;
+    }
+}
